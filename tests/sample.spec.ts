881b1a070298690eac9ebd05204993a79cc9405e
AmjadCodeLab <amjadaliaccen@gmail.com>
2023-12-12 22:58:34 +0530

    12Dec 2023

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,6 +1,15 @@
 import {test,expect} from '@playwright/test'
 
 test('check url',async({page})=>{
-    await page.goto('https://www.cricbuzz.com/');
-    await expect(page).toHaveTitle(/Live Cricket Score/)
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Fast and reliable end-to-end testing for modern web apps | Playwright/)
+
+    await page.locator(':nth-match(:text("API"),1)').click();
+    await expect(page).toHaveTitle(/Playwright Library | Playwright/);
+})
+
+test('check api documentation page',async ({page})=>{
+    await page.goto('https://playwright.dev/docs/api/class-apirequest');
+    await page.locator(':nth-match(:text("APIRequest"),1)').click();
+    await expect(page).toHaveTitle(/APIRequest/);
 })
